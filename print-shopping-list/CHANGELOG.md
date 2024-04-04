3d2ef37845d2a0402ab401667045d485e8b3bd6e
Guilherme Mastria <guilherme@mastria.dev.br>
2024-04-04 14:56:00 -0300

    first commit

---
+++ print-shopping-list/CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## [1.0.3] - Fix deploy
+## [1.0.2] - PHP Update
+
+## [1.0.1] - Fix errors
---
+++ print-shopping-list/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/with-contenv bashio
+
+if ! [[ -v PRINTER_IP ]]; then
+    export PRINTER_IP=$(bashio::config 'printer_ip')
+fi
+
+if ! [[ -v LOG_LEVEL ]]; then
+    export LOG_LEVEL=$(bashio::config 'log_level')
+fi
+
+echo "Running HTTP API..."
+/usr/bin/supervisord -c /home/app/docker-files/supervisord/webapi.conf
