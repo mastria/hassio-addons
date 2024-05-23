cb2404043ef2980fea9e7095b3917417c9136236
Guilherme Mastria <guilherme@mastria.dev.br>
2024-05-23 18:30:05 -0300

    update to laravel 10

--- intelbras-solar/app/Console/Kernel.php
+++ intelbras-solar/app/Console/Kernel.php
@@ -9,22 +9,16 @@ class Kernel extends ConsoleKernel
 {
     /**
      * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('intelbras:verificar-geracao')->hourly()->between('9:00', '18:00');
-        $schedule->command('intelbras:total-gerado')->dailyAt('18:30');
+        $schedule->command('intelbras:verificar-geracao')->everyMinute()->between('6:00', '20:00');
     }
 
     /**
      * Register the commands for the application.
-     *
-     * @return void
      */
-    protected function commands()
+    protected function commands(): void
     {
         $this->load(__DIR__ . '/Commands');
 
