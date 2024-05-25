2ed266a4bef73ac4798322a9535bbc8112132bb1
Guilherme Mastria <guilherme@mastria.dev.br>
2024-05-24 21:51:51 -0300

    update

--- intelbras-solar/run.sh
+++ intelbras-solar/run.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/with-contenv bashio
 
-if ! [[ -v INTELBRAS_USER ]]; then
-    export INTELBRAS_USER=$(bashio::config 'usuario')
+INTELBRAS_USER=$(bashio::config 'usuario')
+INTELBRAS_PASSWORD=$(bashio::config 'senha')
+LOG_LEVEL=$(bashio::config 'log_level')
+
+if [[ -z "$INTELBRAS_USER" ]] || [[ -z "$INTELBRAS_PASSWORD" ]]; then
+    echo "Erro: 'usuario' e 'senha' precisam ser informados na configuração."
+    exit 1
 fi
 
-if ! [[ -v INTELBRAS_PASSWORD ]]; then
-    export INTELBRAS_PASSWORD=$(bashio::config 'senha')
-fi
+export INTELBRAS_USER
+export INTELBRAS_PASSWORD
+export LOG_LEVEL
 
-if ! [[ -v LOG_LEVEL ]]; then
-    export LOG_LEVEL=$(bashio::config 'log_level')
-fi
-
-echo "Running schedules..."
+echo "Running schedules with log level: $LOG_LEVEL..."
+sed -i "s/loglevel=debug/loglevel=$LOG_LEVEL/" /home/app/docker-files/supervisord/app.conf
 /usr/bin/supervisord -c /home/app/docker-files/supervisord/app.conf
